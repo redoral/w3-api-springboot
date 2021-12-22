42b4d27ba918ef19b68b8b02ca2e99d03701ec68
Red Oral <redmunozoral@gmail.com>
2021-12-22 14:21:06 -0800

    Initial commit.

---
+++ src/main/java/com/redoral/w3api/W3apiApplication.java
@@ -0,0 +1,13 @@
+package com.redoral.w3api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W3apiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W3apiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redoral/w3api/W3apiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redoral.w3api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W3apiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
